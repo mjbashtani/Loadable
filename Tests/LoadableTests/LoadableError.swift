5ad1788d153cea5cedc806b9b87b4a5aad3add49
mjb <mohamadjavadbashtani@gmail.com>
2022-01-16 22:25:07 +0330

    Generalize  Loadable error for all testCases

---
+++ Tests/LoadableTests/LoadableError.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mohammad Javad Bashtani on 10/26/1400 AP.
+//
+
+import Foundation
+import XCTest
+
+extension XCTestCase {
+     struct LoadableError: LocalizedError, Identifiable {
+        let id: String = UUID().uuidString
+    }
+}
