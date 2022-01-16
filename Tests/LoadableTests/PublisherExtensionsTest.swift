a93f1ee8502a5acc6d27dd8bb452c8cae7a67a75
mjb <mohamadjavadbashtani@gmail.com>
2022-01-16 22:18:32 +0330

    Crate test case for publisher extensions

---
+++ Tests/LoadableTests/PublisherExtensionsTest.swift
@@ -0,0 +1,14 @@
+//
+//  PublisherExtensionsTest.swift
+//  
+//
+//  Created by Mohammad Javad Bashtani on 10/26/1400 AP.
+//
+
+import Foundation
+import Combine
+import XCTest
+
+final class PublisherExtensionsTest: XCTestCase {
+    
+}
