5bff775fb18e376b02c4082fd5d1dc162e193433
mjb <mohamadjavadbashtani@gmail.com>
2022-01-16 22:25:42 +0330

    Add Publisher's helper method for testing

---
+++ Tests/LoadableTests/XCTest+Extensions.swift
@@ -0,0 +1,69 @@
+//
+//  XCTestCase+Extensions.swift
+//  CityFinderTests
+//
+//  Created by Mohammad Javad Bashtani on 9/11/1400 AP.
+//
+
+import XCTest
+import Foundation
+import Combine
+
+/// Adapted from: https://www.swiftbysundell.com/articles/unit-testing-combine-based-swift-code
+extension XCTestCase {
+    func awaitPublisher<T: Publisher>(
+        _ publisher: T,
+        timeout: TimeInterval = 10,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) throws -> T.Output {
+        // This time, we use Swift's Result type to keep track
+        // of the result of our Combine pipeline:
+        var result: Result<T.Output, Error>?
+        let expectation = self.expectation(description: "Awaiting publisher")
+
+        let cancellable = publisher.sink(
+            receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    result = .failure(error)
+                case .finished:
+                    break
+                }
+
+                expectation.fulfill()
+            },
+            receiveValue: { value in
+                result = .success(value)
+            }
+        )
+
+        // Just like before, we await the expectation that we
+        // created at the top of our test, and once done, we
+        // also cancel our cancellable to avoid getting any
+        // unused variable warnings:
+        waitForExpectations(timeout: timeout)
+        cancellable.cancel()
+
+        // Here we pass the original file and line number that
+        // our utility was called at, to tell XCTest to report
+        // any encountered errors at that original call site:
+        let unwrappedResult = try XCTUnwrap(
+            result,
+            "Awaited publisher did not produce any output",
+            file: file,
+            line: line
+        )
+
+        return try unwrappedResult.get()
+    }
+}
+
+extension Published.Publisher {
+    func collectNext(_ count: Int) -> AnyPublisher<[Output], Never> {
+        self.dropFirst()
+            .collect(count)
+            .first()
+            .eraseToAnyPublisher()
+    }
+}
