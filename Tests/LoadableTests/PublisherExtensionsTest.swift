455b4081376b92406a2fbb693f2a51ae47424f5f
mjb <mohamadjavadbashtani@gmail.com>
2022-01-16 23:31:18 +0330

    Test loadable publishers

--- Tests/LoadableTests/PublisherExtensionsTest.swift
+++ Tests/LoadableTests/PublisherExtensionsTest.swift
@@ -40,4 +40,29 @@ final class PublisherExtensionsTest: XCTestCase {
         XCTAssertEqual(result.first, Loadable<String>.failed(error))
         
     }
+    
+    func test_loadableIsloadingPublisherWhenIsLoading() throws {
+        let result = try awaitPublisher(Just(Loadable<String>.isLoading)
+                                            .isLoading)
+        XCTAssertEqual(result, true)
+    }
+    func test_loadableIsloadingPublisherWhenIsNotLoading() throws {
+        let result = try awaitPublisher(Just(Loadable<String>.loaded(""))
+                                            .isLoading)
+        XCTAssertEqual(result, false)
+    }
+    
+    func test_loadableLoadedPublisher() throws {
+        let value = String.init()
+        let result = try awaitPublisher(Just(Loadable<String>.loaded(value))
+                                            .loaded)
+        XCTAssertEqual(result, value)
+    }
+    
+    func test_loadableFailedPublisher() throws {
+        let error = LoadableError()
+        let result = try awaitPublisher(Just(Loadable<String>.failed(error))
+                                            .isFailed)
+        XCTAssertEqual(result.localizedDescription, error.localizedDescription)
+    }
 }
